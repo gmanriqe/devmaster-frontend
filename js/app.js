728bb8ba2b07104525bd91d3fb6f97d6f4c8a6f9
gmanriqe <gmanriqe@gmail.com>
2018-09-25 01:55:22 -0500

    Banner

--- js/app.js
+++ js/app.js
@@ -1,5 +1,29 @@
-function onReady(){
-   console.log('carga exitosa');
+function onReady() {
+  console.log("carga exitosa");
+  //    $(window).on(scroll,function(){
+  console.log($(window).scrollTop());
+  //    if( >= 200 ) {
+  //        $('#navBar').addClass('fixed-top');
+  //    }else {
+  //     $('#navBar').removeClass('fixed-top');
+  //    }
+  //    })
+
+  $(window).on("scroll", function() {
+    var sTop = $(window).scrollTop();
+    navBar = $("#navBar");
+    if (sTop >= 400) {
+      navBar.addClass("fixed-top");
+      navBar.css({
+        "background-color": "black"
+      });
+    } else {
+      navBar.removeClass("fixed-top");
+      navBar.css({
+        "background-color": "transparent"
+      });
+    }
+  });
 }
 
 $(document).ready(onReady());
